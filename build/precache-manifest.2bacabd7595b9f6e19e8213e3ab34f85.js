698969bddb700652de48365dc2bd41b177374cc2
= <=>
2019-12-02 21:35:30 +0300

    homepage

---
+++ build/precache-manifest.2bacabd7595b9f6e19e8213e3ab34f85.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce4a34b471591034e6c8ff09bf1a5eb",
+    "url": "/femidamurino/index.html"
+  },
+  {
+    "revision": "4a294535a7f9c958396f",
+    "url": "/femidamurino/static/css/main.056f1910.chunk.css"
+  },
+  {
+    "revision": "0bed09ae01ee211a1954",
+    "url": "/femidamurino/static/js/2.ecf6000e.chunk.js"
+  },
+  {
+    "revision": "4a294535a7f9c958396f",
+    "url": "/femidamurino/static/js/main.d98018a2.chunk.js"
+  },
+  {
+    "revision": "3535352266ffe659d47f",
+    "url": "/femidamurino/static/js/runtime-main.373aadae.js"
+  },
+  {
+    "revision": "c3898c60fb0bb5a01f6350eb6afebe32",
+    "url": "/femidamurino/static/media/3ndfl.c3898c60.png"
+  },
+  {
+    "revision": "96c0d6c9142a0159a33f144b633831a2",
+    "url": "/femidamurino/static/media/arrow.96c0d6c9.svg"
+  },
+  {
+    "revision": "6b3dd49950e9dbaf00454aff880aa526",
+    "url": "/femidamurino/static/media/building.6b3dd499.png"
+  },
+  {
+    "revision": "7272bd3dbc61b081aa2cdcdd22af7e1b",
+    "url": "/femidamurino/static/media/building.7272bd3d.svg"
+  },
+  {
+    "revision": "376e77da55bdaa96cd537b4ab20ab61a",
+    "url": "/femidamurino/static/media/burger.376e77da.svg"
+  },
+  {
+    "revision": "598d1cbf0972e36ee5d2f5db8557a9e4",
+    "url": "/femidamurino/static/media/claim.598d1cbf.png"
+  },
+  {
+    "revision": "e47aeb0c49caa0ef27970f54f1c1663c",
+    "url": "/femidamurino/static/media/close.e47aeb0c.svg"
+  },
+  {
+    "revision": "d313d606fb4c1dae5fff5b3af7748c40",
+    "url": "/femidamurino/static/media/company.d313d606.svg"
+  },
+  {
+    "revision": "56c260b02014c0fb27252520ec4e803c",
+    "url": "/femidamurino/static/media/email.56c260b0.svg"
+  },
+  {
+    "revision": "a127394295fe32106a1c0330f7aa2274",
+    "url": "/femidamurino/static/media/email.a1273942.svg"
+  },
+  {
+    "revision": "d023eae25312065b8ffd6c8663290e7a",
+    "url": "/femidamurino/static/media/femida.d023eae2.png"
+  },
+  {
+    "revision": "0c81e791cce63b23525af56d58f0c815",
+    "url": "/femidamurino/static/media/hide.0c81e791.svg"
+  },
+  {
+    "revision": "14ded6b0de6fbcda78dfae1b0b2d4bb0",
+    "url": "/femidamurino/static/media/liqidation.14ded6b0.png"
+  },
+  {
+    "revision": "e0d230a2e54dde90727cfa05e4f85c51",
+    "url": "/femidamurino/static/media/location.e0d230a2.svg"
+  },
+  {
+    "revision": "ddf859f9c01721422a0918e70c934580",
+    "url": "/femidamurino/static/media/logo.ddf859f9.png"
+  },
+  {
+    "revision": "99debb734cc9674fe374d9fc483e08f3",
+    "url": "/femidamurino/static/media/metro.99debb73.svg"
+  },
+  {
+    "revision": "3df18c5ff918a2633eaf421dddd4ae2f",
+    "url": "/femidamurino/static/media/newContract.3df18c5f.png"
+  },
+  {
+    "revision": "90c77bd7cbb756784bb4d32e54a4138f",
+    "url": "/femidamurino/static/media/phone.90c77bd7.svg"
+  },
+  {
+    "revision": "fc82b6f6b01bf86b42703436ce523d5e",
+    "url": "/femidamurino/static/media/phone.fc82b6f6.svg"
+  },
+  {
+    "revision": "59f1a4599403971a56179fa0144d1d46",
+    "url": "/femidamurino/static/media/plus.59f1a459.svg"
+  },
+  {
+    "revision": "b5944278b9e3527252b16511d50a6b38",
+    "url": "/femidamurino/static/media/register.b5944278.png"
+  },
+  {
+    "revision": "88bb1df0c871901f56bb733763f07ce0",
+    "url": "/femidamurino/static/media/representation.88bb1df0.png"
+  },
+  {
+    "revision": "507cb8ae4c8d3a297c227d915574c35d",
+    "url": "/femidamurino/static/media/timer.507cb8ae.svg"
+  },
+  {
+    "revision": "7cbe023718b74377f0a919195a3e410b",
+    "url": "/femidamurino/static/media/user.7cbe0237.svg"
+  },
+  {
+    "revision": "cdee55b9cc40061897180f35585a7843",
+    "url": "/femidamurino/static/media/vk.cdee55b9.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.09f488ba63b6eaf89b1c2a5ce0844257.js"
+  "/femidamurino/precache-manifest.2bacabd7595b9f6e19e8213e3ab34f85.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/femidamurino/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
